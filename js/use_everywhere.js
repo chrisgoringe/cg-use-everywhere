eb97eb2433980157a70bf4063530397258beaaad
Chris <chris.goringe@gmail.com>
2023-09-27 23:15:55 +1000

    Much simplified

--- js/use_everywhere.js
+++ js/use_everywhere.js
@@ -9,32 +9,16 @@ app.registerExtension({
             const nodes = p.workflow.nodes;
             var use_everywheres = {};
             nodes.forEach(node => {
-                if (node?.type.startsWith('UE ')) {
-                    use_everywheres[node.type.substring(3)] = node;
+                if (node.type.startsWith('UE ')) {
+                    use_everywheres[node.type.substring(3)] = node.id.toString();
                 }
             })
             nodes.forEach(node => {
-                if (node.inputs) {
-                    for (var i=0; i<node.inputs.length; i++) {
-                        var input = node.inputs[i];
-                        if (input.link === null) {
-                            var ue = use_everywheres[input.type];
-                            var ue_id = ue.id.toString();
-                            // create a new link
-                            //p.workflow.last_link_id += 1; 
-                            //const new_link = [p.workflow.last_link_id, ue_id, 0, node.id, i, input.type];
-                            //p.workflow.links.splice(p.workflow.links.length,0,new_link);
-
-                            // set it in the workflow
-                            //input.link = p.workflow.last_link_id;
-                            // in the output
-                            p.output[node.id].inputs[input.name] = [ue_id,0];
-                            // and on the output node in the workflow
-                            //if (ue.outputs[0].links) { ue.outputs[0].links.splice(0,0,p.workflow.last_link_id)}
-                            //else { ue.outputs[0].links = [p.workflow.last_link_id] }
-                        }
-                    };
-                }
+                node.inputs?.forEach(input => {
+                    if (input.link === null && use_everywheres[input.type]) {
+                        p.output[node.id].inputs[input.name] = [use_everywheres[input.type],0];
+                    }
+                });
             });
             return p;
         }
