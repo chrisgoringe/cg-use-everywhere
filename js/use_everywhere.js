7db5df5535a3aca232f2b5469d1d6b0cacfdb0ca
Chris <chris.goringe@gmail.com>
2023-10-01 23:21:22 +1100

    Set the node color to match the type - issue 9

--- js/use_everywhere.js
+++ js/use_everywhere.js
@@ -75,18 +75,19 @@ app.registerExtension({
         if (nodeType.title.startsWith("Anything Everywhere")) {
             const onConnectionsChange = nodeType.prototype.onConnectionsChange;
             nodeType.prototype.onConnectionsChange = function (side,slot,connect,link_info,output) {
-				onConnectionsChange?.apply(side,slot,connect,link_info,output);
                 if (connect && link_info) {
                     const origin_id = link_info.origin_id;
                     const origin_slot = link_info.origin_slot;
                     const input = this.graph._nodes_by_id[origin_id].outputs[origin_slot];
                     this.input_type = input.type;
                     this.inputs[0].name = this.input_type;
+                    this.inputs[0].color_on = app.canvas.default_connection_color_byType[input.type];
                 } else {
                     this.input_type = undefined;
+                    this.inputs[0].color_on = undefined;
                     this.inputs[0].name = "anything";
                 }
-                
+                onConnectionsChange?.apply(side,slot,connect,link_info,output);
             };
         }
     },
