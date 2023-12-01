fbd48c1337c5c9e7f93ff1e29a4f82ce6ed200f1
Chris <chris.goringe@gmail.com>
2023-12-01 14:27:39 +1100

    use new javascript directory

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .use_everywhere import SeedEverywhere, AnythingEverywherePrompts
 
-UE_VERSION = "4.3.5"
+UE_VERSION = "4.3.7"
 
 NODE_CLASS_MAPPINGS = { "Seed Everywhere": SeedEverywhere }
 
@@ -10,16 +10,20 @@ NODE_CLASS_MAPPINGS["Anything Everywhere3"] = AnythingEverywhereTriplet
 NODE_CLASS_MAPPINGS["Anything Everywhere?"] = AnythingSomewhere
 NODE_CLASS_MAPPINGS["Prompts Everywhere"] = AnythingEverywherePrompts
 
-__all__ = ['NODE_CLASS_MAPPINGS']
-
 import os, shutil
 import folder_paths
+
+# temporary code to remove old javascript installs
 module_js_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "js")
 application_root_directory = os.path.dirname(folder_paths.__file__)
-extension_web_extensions_directory = os.path.join(application_root_directory, "web", "extensions", "use_everywhere")
-
-shutil.copytree(module_js_directory, extension_web_extensions_directory, dirs_exist_ok=True)
+old_code_location = os.path.join(application_root_directory, "web", "extensions", "use_everywhere")
+if os.path.exists(old_code_location):
+    shutil.rmtree(old_code_location)
 
 old_code_location = os.path.join(application_root_directory, "web", "extensions", "cg-nodes", "use_everywhere.js")
 if os.path.exists(old_code_location):
     os.remove(old_code_location)
+# end of temporary code
+
+WEB_DIRECTORY = "./js"
+__all__ = ["NODE_CLASS_MAPPINGS", "WEB_DIRECTORY"]
