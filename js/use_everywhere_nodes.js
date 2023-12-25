2a6a45172d82eb65016bcb4f2f581d482a3828ee
mijuku233 <89350747+mijuku233@users.noreply.github.com>
2023-12-25 18:12:31 +0800

    Update use_everywhere_nodes.js

--- js/use_everywhere_nodes.js
+++ js/use_everywhere_nodes.js
@@ -53,7 +53,7 @@ function add_ue_from_node(ues, node) {
             if (in_link) {
                 const type = app.graph._nodes_by_id[node.id.toString()]?.input_type[i];
                 const link = handle_bypass(app.graph.links[in_link], type);
-                if (link) ues.add_ue(node, i, type, [link.origin_id.toString(), link.origin_slot], undefined, new RegExp(["(^prompt|^positive)","neg"][i]), 5);
+                if (link) ues.add_ue(node, i, type, [link.origin_id.toString(), link.origin_slot], undefined, new RegExp(["^(?!.*neg|.*负面)","neg|负面"][i]), 5);
             }
         }
     }
