2145fd20b3e860c913c6f4258c46442f94776d52
Chris <chris.goringe@gmail.com>
2024-03-12 13:05:20 +1100

    fix #110

--- js/use_everywhere.js
+++ js/use_everywhere.js
@@ -64,8 +64,9 @@ app.registerExtension({
                 if (this.type=="Anything Everywhere?" && slot!=0) {
                     // don't do anything for the regexs
                 } else {
-                    this.input_type[slot] = (connect && link_info) ? get_real_node(link_info?.origin_id)?.outputs[link_info?.origin_slot]?.type 
-                                                                : undefined;
+                    const type = (connect && link_info) ? get_real_node(link_info?.origin_id)?.outputs[link_info?.origin_slot]?.type : undefined;
+                    this.input_type[slot] = type;
+                    link_info.type = type ? type : "*";
                     update_input_label(this, slot, app);
                 }
             }
