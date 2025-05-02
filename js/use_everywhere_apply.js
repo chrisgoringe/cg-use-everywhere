c0ed3c73d451ba20fa2129fe3217507c52afed8e
Chris <chris.goringe@gmail.com>
2025-05-02 15:50:50 +1000

    #295

--- js/use_everywhere_apply.js
+++ js/use_everywhere_apply.js
@@ -3,7 +3,7 @@ import { display_name } from "./use_everywhere_classes.js";
 import { is_UEnode, get_real_node, Logger } from "./use_everywhere_utilities.js";
 
 
-function _convert_to_links(ue, added_links) {
+function _convert_to_links(ue, added_links, removed_links) {
     const output_node_id = ue.output[0];
     const output_index = ue.output[1];
     const output_node = get_real_node(output_node_id);
@@ -12,25 +12,36 @@ function _convert_to_links(ue, added_links) {
         const input_node_id = st.node.id;
         const input_node = get_real_node(input_node_id);
         const input_index = st.input_index;
+        if (input_node.inputs[input_index].link) {
+            const llink = app.graph.links[input_node.inputs[input_index].link]
+            removed_links.push( {...llink} )
+        }
         const new_link = output_node.connect(output_index, input_node, input_index);
         if (!new_link)
             console.error("Failed to connect nodes: " +
                           `${output_node_id}[${output_index}] -> ` +
                           `${input_node_id}[${input_index}].`);
         else { // Memorize the links we are adding to remove them later
-            if (added_links)
-                added_links.push(new_link.id);
+            if (added_links) added_links.push(new_link.id);
             Logger.log(Logger.INFORMATION, `  -> ${display_name(st.node)}, ${st.input.name} ` +
                                            `(${st.node.id}.${st.input_index}) (ID: ${new_link.id})`);
         }
     });
 }
 
-function convert_to_links(ues, control_node_id, added_links=undefined) {
+function convert_to_links(ues, control_node_id) {
+    const added_links = []
+    const removed_links = []
     ues.ues.forEach((ue)=> {
-        if (control_node_id==-1 || ue.controller.id == control_node_id) _convert_to_links(ue, added_links);
+        if (control_node_id==-1 || ue.controller.id == control_node_id) _convert_to_links(ue, added_links, removed_links);
     });
-    return added_links;
+    const restorer = function() {
+        added_links.forEach(id => { app.graph.removeLink(id); });
+        removed_links.forEach(llink => {
+            app.graph._nodes_by_id[llink.origin_id].connect(llink.origin_slot, app.graph._nodes_by_id[llink.target_id], llink.target_slot)
+        })
+    };
+    return {restorer:restorer, added_links:added_links}
 }
 
 function remove_this(node, keep_seed_everywhere) {
