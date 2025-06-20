b41d403619305b233d1fa93eba2d771139aec48e
Chris <chris.goringe@gmail.com>
2025-06-20 11:01:38 +1000

    enable callbacks when setting changes

--- js/use_everywhere_cache.js
+++ js/use_everywhere_cache.js
@@ -3,17 +3,22 @@ import { app } from "../../scripts/app.js";
 class LocalSettingsCache {
     constructor() {
         this.local_settings = {}
+        this.callbacks = {}
     }
     getSettingValue(key) {
         if (this.local_settings[key]===undefined) this.local_settings[key] = app.ui.settings.getSettingValue(key)
         return this.local_settings[key]
     }
     onSettingChangeChange(new_value, old_value) {
-        settingsCache.local_settings[this.id] = new_value
-        app.graph?.change.bind(app.graph)
+        this.onSettingChange(new_value, old_value)
+        app.graph?.change.bind(app.graph)()
     }
     onSettingChange(new_value, old_value) {
         settingsCache.local_settings[this.id] = new_value
+        this.callbacks[this.id]?.(new_value, old_value)
+    }
+    addCallback(id, callback) {
+        this.callbacks[id] = callback
     }
 }
 
