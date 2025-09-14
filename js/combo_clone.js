44e074cb12fe63fd1af279e71e50383d5196d877
Chris <chris.goringe@gmail.com>
2025-09-14 11:19:53 +1000

    combo in subgrapoh

--- js/combo_clone.js
+++ js/combo_clone.js
@@ -1,5 +1,4 @@
 import { shared } from "./shared.js"
-import { app } from "../../scripts/app.js";
 
 function update_me(node) {
     node.widgets[0].options.values = [...node.properties.comboclone.options]
@@ -13,7 +12,8 @@ export function reset_comboclone_on_load(node) {
 
 export function comboclone_on_connection(node, link_info) {
     if (shared.graph_being_configured) return
-    const target_node = app.graph.getNodeById(link_info.target_id)
+    if (!link_info) return
+    const target_node = node.graph.getNodeById(link_info.target_id)
     const input_name = target_node?.inputs[link_info.target_slot].name
     const widget = target_node?.widgets?.find((w)=>(w.name==input_name))
     
