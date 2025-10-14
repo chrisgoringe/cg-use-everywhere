210b77b0151e7e0e3c65cd5f97b80f0c09d5a213
Chris <chris.goringe@gmail.com>
2025-10-15 09:46:41 +1100

    version to 7.4

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .use_everywhere import SeedEverywhere, AnythingEverywherePrompts
 from typing import Any
 
-UE_VERSION = "7.3"
+UE_VERSION = "7.4"
 
 NODE_CLASS_MAPPINGS:dict[str,Any] = { "Seed Everywhere": SeedEverywhere }
 
